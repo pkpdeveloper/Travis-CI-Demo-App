4dfdcfbe1c0e46d1fdbe48f84fa8968f0f919713
Pankaj Kant Patel <pankaj.patel@globant.com>
2019-08-22 11:33:51 +0530

    Move file

--- android_wait_for_emulator.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Copied from: https://github.com/travis-ci/travis-cookbooks/blob/precise-stable/ci_environment/android-sdk/files/default/android-wait-for-emulator
-
-
-
-echo "Emulator is ready"
